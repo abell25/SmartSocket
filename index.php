482fd767ad845b43e8d0c28746cf23746383e2e3
nbonnet <thebonnet09@yahoo.com>
2013-10-15 20:49:21 -0500

    Added files for simple db connectivity

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$link = mysqli_connect('IP', 'USERNAME', 'PASSWORD');
+if (!$link)
+{
+	$output = 'Unable to connect to the database server.';
+	include 'output.html.php';
+	exit();
+}
+
+if (!mysqli_set_charset($link, 'utf8'))
+{
+	$output = 'Unable to set database connection encoding.';
+	include 'output.html.php';
+	exit();
+}
+
+if (!mysqli_select_db($link, 'smartsocket'))
+{
+	$output = 'Unable to locate the smartsocket database.';
+	include 'output.html.php';
+	exit();
+}
+
+$output = 'Database connection established.';
+include 'output.html.php';
+?>

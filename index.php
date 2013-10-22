db7434d6872c3bedbe7ed1f4db7727929913186d
nbonnet <thebonnet09@yahoo.com>
2013-10-22 18:53:16 -0500

    attempt to connect to the database we created

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect('IP', 'USERNAME', 'PASSWORD');
+$link = mysqli_connect('http://yoursmartsocket.com/', 'root', 'bitnami');
 if (!$link)
 {
 	$output = 'Unable to connect to the database server.';

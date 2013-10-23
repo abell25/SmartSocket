04da8f76e72d7a2ade292c09c0e754ce0d43b592
nbonnet <thebonnet09@yahoo.com>
2013-10-23 18:37:57 -0500

    Revert "fix"
    This reverts commit 4a6b90f88b54637672242558e2f6458386370268.

--- index.php
+++ index.php
@@ -80,9 +80,37 @@ if (isset($_POST['action']) and $_POST['action'] == 'register_form')
 	exit();
 }
 /*************************************************************************/
+
 if (isset($_GET['login']))
 {
+	$action = 'login_form'
 	include 'login.html.php';
+	exit();
+}
+
+if (isset($_POST['action']) and $_POST['action'] == 'login_form' )
+{
+	$username = mysqli_real_escape_string($connection, $_POST['username']);
+	$password = mysqli_real_escape_string($connection, $_POST['password']);
+	
+	$output = 'Username: ' . $username . ' Password: ' . $password;
+	
+	$sql = 'SELECT COUNT(*) FROM user WHERE username="'.$username.'" AND password="'.$password.'"';
+	$result = mysqli_query($link, $sql);
+	if (!$result)
+	{
+		$error = 'Error seeing if user exists in the DB.';
+		include 'output.html.php';
+		exit();
+	}	
+	
+	if($result > 0) {
+		$output .= '\nAccess Granted!';
+	} else {
+		$output .= '\nAccess Denied! Please register first...';
+	}
+	
+	include 'output.html.php';
 	exit();
 }
 

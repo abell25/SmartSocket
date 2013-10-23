077a503d39c151be7b56dac289eb08494085773a
nbonnet <thebonnet09@yahoo.com>
2013-10-23 17:59:17 -0500

    Fixes

--- login.html.php
+++ login.html.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>	
 		<div id="content">
-			<form id="registration_form" method="POST" action="?login">
+			<form id="registration_form" method="POST" action="login">
 				<label for="username-id">Username:</label>
 				<input id="username-id" type="text" name="username" autofocus="autofocus" /><br/><br/>
 				

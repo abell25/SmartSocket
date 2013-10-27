f94e9e5c0072f91ac0948e35bbc072aceb3a0632
nbonnet <thebonnet09@yahoo.com>
2013-10-26 19:12:51 -0500

    Fields now required
    Made fields on login page and registration page required

--- login.html.php
+++ login.html.php
@@ -10,10 +10,10 @@
 		<div id="content">
 			<form id="registration_form" method="POST" action="?login">
 				<label for="username-id">Username:</label>
-				<input id="username-id" type="text" name="username" autofocus="autofocus" /><br/><br/>
+				<input id="username-id" type="text" name="username" autofocus="autofocus" required="required"/><br/><br/>
 				
 				<label for="password1-id">Password:</label>
-				<input id="password1-id" class="text-input" type="password" name="password" /><br/><br/>
+				<input id="password1-id" class="text-input" type="password" name="password" required="required"/><br/><br/>
 				
 				<input type="submit" value="Login"/>
 			</form>
--- register.html.php
+++ register.html.php
@@ -11,16 +11,16 @@
 			<form id="registration_form" method="POST" action="?register">
 
 				<label for="username-id">Username:</label>
-				<input id="username-id" type="text" name="username" autofocus="autofocus" /><br/><br/>
+				<input id="username-id" type="text" name="username" required="required" autofocus="autofocus" /><br/><br/>
 				
 				<label for="email-id">Email:</label>
-				<input id="email-id" type="email" name="email" /><br/><br/>
+				<input id="email-id" type="email" required="required" name="email" /><br/><br/>
 				
 				<label for="password1-id">Password:</label>
-				<input id="password1-id" class="text-input" type="password" name="password" /><br/><br/>
+				<input id="password1-id" class="text-input" required="required" type="password" name="password" /><br/><br/>
 				
 				<label for="password2-id">Retype Password:</label>
-				<input id="password2-id" class="text-input" type="password" name="password_repeat" /><br/><br/>
+				<input id="password2-id" class="text-input" required="required" type="password" name="password_repeat" /><br/><br/>
 				
 				<input type="submit" value="Register"/>
 			</form>

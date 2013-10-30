489359310db61f953d05cf8598ffc752bf90cdc6
nbonnet <thebonnet09@yahoo.com>
2013-10-29 20:12:35 -0500

    specific smart socket page
    Created a page to show information for a specific smart socket

--- index.php
+++ index.php
@@ -54,6 +54,34 @@ if(isset($_GET['login'])) {
 		include 'register.html.php';
 		exit();
     }
+} elseif (isset($_GET['specific']) and isset($_GET['id'])) {
+	$id = $_GET['id'];
+	$pagetitle = "Device page for ". $id;
+
+	$sql = "SELECT nickname,schedule_last_modified,
+				max_power_usage,max_cost,user_set_state
+				,user.username AS owner FROM device,user'
+				WHERE device.user_id = user.user_id AND dev_id ='". $id."'";
+	$result = mysqli_query($connection, $sql);
+	if (!$result) {
+			$error = 'Error finding info for device with id='.$id.': ' . mysqli_error($connection);
+			include 'output.html.php';
+			exit();
+	}
+	$row = mysqli_fetch_array($result);
+	
+	$output .='<ul>';
+		$output .='<li>Device ID: '.$id.'</li>';
+		$output .='Device Nickname: '.$row['nickname'].'</li>';
+		$output .='Schedule was last modified: '.$row['schedule_last_modified'].'</li>';
+		$output .='Devices Set MAX power usage: '.$row['max_power_usage'].'</li>';
+		$output .='Devices Set MAX cost: '.$row['max_cost'].'</li>';
+		$output .='The user selected state for this device: '.$row['user_set_state'].'</li>';
+		$output .='Owner: '.$row['owner'].'</li>';
+	$output .='</ul>';
+	
+	include 'specific_device_page.html.php';
+	exit();
 } else {  
     include '404.html.php';  // No page found
 }
---
+++ specific_device_page.html.php
@@ -0,0 +1,14 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] .
+		'/SmartSocket/includes/helpers.inc.php'; ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php htmlout($pagetitle); ?></title>
+		<!--link type="text/css" rel="stylesheet" href="stylesheet.css"/-->
+	</head>
+	<body>	
+		<div id="content">
+			<?php htmlout($output); ?>
+		</div>
+	</body>
+</html>

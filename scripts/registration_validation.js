35fca5844c4831957c40e729c31c385892e6350f
nbonnet <thebonnet09@yahoo.com>
2013-10-29 19:14:29 -0500

    Attempted some validation

--- scripts/registration_validation.js
+++ scripts/registration_validation.js
@@ -4,14 +4,37 @@ $(document).ready(function(){
 	});
 	
 	var validateUsername = function(event) {
+		clearErrorText();
 		var username = $('#username-id').val();
-		if(username.length > 50) {
-			var error = $("#error_text");
-			error.val(error.val() + "<br /> The username must less than 50 characters.");
-			error.show();
-			$("#submit_button").attr('disabled', true);
-		} else {
-			$("#submit_button").attr('disabled', false);
+		var foundError = false;
+		
+		if(username.length > 0) {
+			if(username.length > 50) {
+				var text = "The username must be less than 50 characters.";
+				setErrorText(text);
+				foundError = true;
+			} 
+			if(!username.match(/^[a-zA-Z0-9]+/)) {
+				var text = "All username characters must be alphanumeric.";
+				setErrorText(text);
+				foundError = true;
+			} 
+			if(!foundError) { 
+				$("#submit_button").attr('disabled', false);
+			}
 		}
 	} 
+	
+	var clearErrorText = function() {
+		var error = $("#error_text");
+		error.text("");
+		error.hide();
+	}
+	
+	var setErrorText = function(text) {
+		var error = $("#error_text");
+		error.text(error.text() + " " + text);
+		error.show();
+		$("#submit_button").attr('disabled', true);
+	}
 });

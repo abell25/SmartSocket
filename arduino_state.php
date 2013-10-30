ddc05ffeaae3dafbb0633676c516ed24c0073dd6
Rjbur <rjbur@uwm.edu>
2013-10-29 20:00:43 -0500

    May have fixed it

--- arduino_state.php
+++ arduino_state.php
@@ -6,17 +6,16 @@ if (isset($_GET['id']))
 {
 	$id = $_GET['id'];
 	// Should use user id to get user_set_state from device table
-	//$output = 'ID: ' . $id . ' Current: ' . $I . ' Voltage: ' . $V . ' Time: ' . $time . ' State: ' . $state; //print to page
-	// This is broken :/
-	$data = mysqli_query($connection,'SELECT user_set_state from device(dev_id) VALUES ('. $id.')' )
+
+	$data = mysqli_query($connection,'SELECT user_set_state from device where dev_id = '.$id )
 	or die(mysql_error());
 	
 	$info = mysql_fetch_array($data);
 	
-	$output 'User State: ' .$info['user_set_state'];.
+	$output 'User State: ' .$info['user_set_state'];
 			 
-
-	include 'output.html.php';
+	echo $output
+	//include 'output.html.php';
 	exit();
 }
 ?>

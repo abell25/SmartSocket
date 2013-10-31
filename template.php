b3bdd17246c22af81ccf101e5668ba9d55440837
Ubuntu <server>
2013-10-31 15:21:54 +0000

    added jquery and knockout.js to the template

--- template.php
+++ template.php
@@ -6,13 +6,15 @@ function PrintHeader($P) { ?>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,800' rel='stylesheet' type='text/css'>
     <link href="http://www.yoursmartsocket.com/SmartSocket/styles/style.css" rel="stylesheet" type="text/css" />
     <link href="http://www.yoursmartsocket.com/SmartSocket/styles/devices.css" rel="stylesheet"type="text/css" />
+    <script type="text/javascript" src="http://www.yoursmartsocket.com/SmartSocket/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="http://www.yoursmartsocket.com/SmartSocket/js/knockout-3.0.0.js"></script>
     <?php
     if (array_key_exists('css', $P)) {
       echo '<link href="http://www.yoursmartsocket.com/SmartSocket/styles/'         . $P['css'] .'" rel="stylesheet" type="text/css" />';
     }
-    /*if (array_key_exists('js', $P)) {
+    if (array_key_exists('js', $P)) {
       echo '<script type="text/javascript" src="http://www.yoursmartsocket.com/SmartSocket/js/' . $P['js'] .'"></script>';
-    }*/
+    }
     ?>
     <title><?php attr($P, 'title');  ?></title>
   </head>

1429142d3cc6edbcac11f41a0351102950a6289e
Ubuntu <server>
2013-10-31 16:05:38 +0000

    set up getDevices.php page to use knockout.js

--- getDevices.php
+++ getDevices.php
@@ -1,42 +1,30 @@
 <?php
 //echo "getting sockets for user..<br />";
 include 'template.php';
-$user_id = $_GET['user_id'];
-
-$conn = mysql_connect('localhost','root','bitnami') or die(mysql_error());
-mysql_select_db("SmartSocket") or die(mysql_error());
-
-$sql = "SELECT dev_id, nickname, schedule, max_power_usage, max_cost, user_set_state from device where user_id=$user_id";
-
-//echo "sql: " . $sql . "<br />";
-
-$result = mysql_query($sql) or die(mysql_error());
 $P = array('title' => 'Devices', 
 	   'css' => 'getDevices.css',
 	   'js'  => 'getDevices.js');
 PrintHeader($P);
-echo '<ul id="sockets_list">';
-while($row = mysql_fetch_assoc($result)) {
-  $dev_id =  $row['dev_id'];
-  $nickname = $row['nickname'];
-  $state = $row['user_set_state'];
-  $max_cost = $row['max_cost']; 
 ?>
+<script>
+user_id = <?php echo $_GET['user_id'] ?>;
+the_data = <?php include 'php_scripts/getDevices.php'; ?>;
+</script>
+<ul data-bind="foreach: devices">
 <div class="content-section">
-  <h2>Device <?php echo $dev_id;  ?></h2>
-  <div class="onoffswitch" id="dev_id_div<?php echo $dev_id; ?>">
-    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="dev_id_<?php echo $dev_id; ?>" <?php echo ($state)? "checked": ""; ?>>
-    <label class="onoffswitch-label" for="dev_id_<?php echo $dev_id; ?>">
+  <h2>Device <span data-bind="text: dev_id"></span></h2>
+  <div class="onoffswitch" data-bind="attr: { 'id': dev_id }">
+    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox">
+    <label class="onoffswitch-label" data-bind="attr: { 'for': dev_id }">
       <div class="onoffswitch-inner"></div>
       <div class="onoffswitch-switch"></div>
     </label>
   </div>
   <div class="link-separator"></div>
   <h1>Max Cost: </h1>
-  <p><?php echo $max_cost; ?></p>
+  <p data-bind="text: max_cost"></p>
 </div>
+</ul>
 <?php
-}
-echo '</ul>';
 PrintFooter();
 ?>
--- js/getDevices.js
+++ js/getDevices.js
@@ -1,1 +1,35 @@
-var test = 2345;
+$(function() {
+    console.log("Started..");
+    
+    ko.applyBindings(new DeviceViewModel());
+});
+
+function Device(data) {
+    var self = this;
+    self.dev_id = data.dev_id;
+    self.nickname = data.nickname;
+    self.schedule = data.schedule;
+    self.max_power_usage = data.max_power_usage;
+    self.max_cost = data.max_cost;
+    self.user_set_state = data.user_set_state;
+}
+
+function DeviceViewModel() {
+    var self = this;
+
+    self.devices = ko.observableArray(the_data);
+
+    self.update = function() {
+	console.log("user_id is " + user_id);
+	$.ajax({
+	    url: "http://yoursmartsocket.com/SmartSocket/php_scripts/getDevices.php",
+	    dataType: "json",
+	    context: this,
+	    data: {'user_id': user_id}
+	}).done(function(resp) {
+	    console.log("ajax returned!");
+	    self.devices(resp);
+	});
+    };
+
+}

bef099e1e23c32f59b0e7f6bc0012c632f4a621d
nbonnet13 <thebonnet09@yahoo.com>
2013-11-01 17:01:44 -0500

    fixed stupid mistakes

--- arduino_state.php
+++ arduino_state.php
@@ -4,11 +4,8 @@ include $_SERVER['DOCUMENT_ROOT'] . '/SmartSocket/includes/db_connection.php';
 
 if (isset($_GET['id'])) {
 	$id = $_GET['id'];
-	// Should use user id to get user_set_state from device table
-
-	$sql = mysqli_query($connection,'SELECT user_set_state from device where dev_id = "'.$id.'"' );
+	$result = mysqli_query($connection,'SELECT user_set_state from device where dev_id = "'.$id.'"');
 	
-	$result = mysqli_query($connection, $sql);
 	if (!$result){
 		$error = 'Error finding arduino state.';
 		include 'output.html.php';
@@ -16,7 +13,6 @@ if (isset($_GET['id'])) {
 	}
 	
 	$info = mysql_fetch_assoc($data);
-	
 	$output 'User State: ' .$info['user_set_state'];
 			 
 	echo $output;

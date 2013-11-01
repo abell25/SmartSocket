d632c1c427669082dc0492c016991734621d1757
nbonnet13 <thebonnet09@yahoo.com>
2013-11-01 13:44:23 -0500

    Spiffed up the login slightly more
    Integrated it into the template and also added some more style

--- login.html.php
+++ login.html.php
@@ -1,22 +1,16 @@
-<?php include_once $_SERVER['DOCUMENT_ROOT'] .
-		'/SmartSocket/includes/helpers.inc.php'; ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Login</title>
-		<!--link type="text/css" rel="stylesheet" href="stylesheet.css"/-->
-	</head>
-	<body>	
-		<div id="content">
-			<form id="login_form" method="POST" action="?login">
-				<label for="username-id">Username:</label>
-				<input id="username-id" type="text" name="username" autofocus="autofocus" required="required"/><br/><br/>
-				
-				<label for="password1-id">Password:</label>
-				<input id="password1-id" class="text-input" type="password" name="password" required="required"/><br/><br/>
-				
-				<input type="submit" value="Login"/>
-			</form>
-		</div>
-	</body>
-</html>
+<?php 
+	include_once $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/includes/helpers.inc.php';
+	include 'template.php';
+	$P = array('title' => 'Login', 
+		   'css' => 'login.css');
+	PrintHeader($P);
+?>
+<form id="login_form" method="POST" action="?login">
+	<label for="username-id">Username:</label>
+	<input id="username-id" type="text" name="username" autofocus="autofocus" required="required"/><br/><br/>
+	
+	<label for="password1-id">Password:</label>
+	<input id="password1-id" class="text-input" type="password" name="password" required="required"/><br/><br/>
+	
+	<input type="submit" value="Login"/>
+</form>

d980b61aac01596c21d2fb3386c478cdaee27112
nbonnet13 <thebonnet09@yahoo.com>
2013-11-02 14:38:12 -0500

    Changed login.html.php back

--- login.html.php
+++ login.html.php
@@ -1,31 +1,11 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/includes/helpers.inc.php';
-include 'template.php';
-$P = array('title' => 'Login', 
-	   'css' => 'login_registeration.css');
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-$error = ' ';
-if (isset($_POST['username']) and isset($_POST['password'])){
-  $conn = mysql_connect('localhost','root','bitnami') or die(mysql_error());
-  mysql_select_db("SmartSocket") or die(mysql_error());
-  $sql = sprintf("SELECT * FROM user WHERE username='%s' and password='%s'", 
-		 mysql_real_escape_string($username), mysql_real_escape_string($password));
-  $result = mysql_query($sql) or die(mysql_error());
-
-  if(mysql_num_rows($result)==0) { 
-    $error = 'incorrect username/password!'; 
-  } else {
-    $user = mysql_fetch_assoc($result);
-    header('Location: http://www.yoursmartsocket.com/SmartSocket/getDevices.php?user_id=' . $user['user_id']);
-  }  
-}
+	include_once $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/includes/helpers.inc.php';
+	include 'template.php';
+	$P = array('title' => 'Login', 
+		   'css' => 'login.css');
+	PrintHeader($P);
 ?>
-<?php PrintHeader($P); ?>
-
 	<form id="login_form" method="POST" action="?login">
-		<p id="error_text"><?php echo $error ?></p><br/>
 		<label for="username-id">Username:</label>
 		<input id="username-id" type="text" name="username" autofocus="autofocus" required="required"/><br/><br/>
 		
@@ -36,5 +16,3 @@ if (isset($_POST['username']) and isset($_POST['password'])){
 	</form>
   </body>
 </html>
-
-<?php PrintFooter($P); ?>

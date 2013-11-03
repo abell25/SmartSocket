5c6d989eabe7315d665f755f55da9edc8e06e35c
Ubuntu <server>
2013-11-03 00:12:37 +0000

    php script had an error when GET parameters were empty

--- php_scripts/updateDevices.php
+++ php_scripts/updateDevices.php
@@ -8,15 +8,15 @@ mysql_select_db("SmartSocket") or die(mysql_error());
 
 $sql = 'UPDATE device set ';
 $columns = array();
-if (isset($_GET['nickname']))
+if (isset($_GET['nickname']) and !empty($_GET['nickname']))
   array_push($columns, "nickname = '" . $_GET['nickname'] . "'"); 
-if (isset($_GET['schedule']))
+if (isset($_GET['schedule']) and !empty($_GET['schedule']))
   array_push($columns, "schedule = '" . $_GET['schedule'] . "'"); 
-if (isset($_GET['max_power_usage']))
+if (isset($_GET['max_power_usage']) and !empty($_GET['max_power_usage']))
   array_push($columns, "max_power_usage = " . $_GET['max_power_usage'] . ""); 
-if (isset($_GET['max_cost']))
+if (isset($_GET['max_cost']) and !empty($_GET['max_cost']))
   array_push($columns, "max_cost = " . $_GET['max_cost'] . ""); 
-if (isset($_GET['user_set_state']))
+if (isset($_GET['user_set_state']) and !empty($_GET['user_set_state']))
   array_push($columns, "user_set_state = " . $_GET['user_set_state'] . ""); 
 
 $sql = $sql . implode(", ", $columns) . " where dev_id = $dev_id";

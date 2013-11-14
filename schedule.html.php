f19323ea607e20bde49f6786c72f0af8f7e14976
pcrone <pjcrone@uwm.edu>
2013-11-14 03:46:46 -0600

    Fixed schedule page setup

--- schedule.html.php
+++ schedule.html.php
@@ -9,36 +9,33 @@
 </head>
 -->
 <?php
-	include_once $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/includes/helpers.inc.php';
 	include 'template.php';
 	$P = array('title' => 'Scheduler', 
 		   'css' => 'schedule.css',
 		   'js'  => 'dhtmlxscheduler.js');
 	PrintHeader($P);
 ?>
-		<div id="main">
-			<div id="content-title">
-				<div id="left-separator" class="separator"></div>
-				<h1>Schedule</h1>
-				<div id="right-separator" class="separator"></div>
-			</div>
-			<div class="content-section">
-				<div id="scheduler_here" class="dhx_cal_container" style='width:100%; height:500px; padding:10px;'>
-				    <div class="dhx_cal_navline">
-					<div class="dhx_cal_prev_button">&nbsp;</div>
-					<div class="dhx_cal_next_button">&nbsp;</div>
-					<div class="dhx_cal_today_button"></div>
-					<div class="dhx_cal_date"></div>
-					<div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
-					<div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
-					<div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
-				    </div>
-				    <div class="dhx_cal_header"></div>
-				    <div class="dhx_cal_data"></div>       
-				</div>
-				<script>scheduler.init('scheduler_here', new Date(),"month");</script>
-			</div>			
+	<div id="main">
+		<div id="content-title">
+			<div id="left-separator" class="separator"></div>
+			<h1>Schedule</h1>
+			<div id="right-separator" class="separator"></div>
 		</div>
+		<div class="content-section">
+			<div id="scheduler_here" class="dhx_cal_container" style='width:100%; height:500px; padding:10px;'>
+				<div class="dhx_cal_navline">
+				<div class="dhx_cal_prev_button">&nbsp;</div>
+				<div class="dhx_cal_next_button">&nbsp;</div>
+				<div class="dhx_cal_today_button"></div>
+				<div class="dhx_cal_date"></div>
+				<div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
+				<div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
+				<div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
+				</div>
+				<div class="dhx_cal_header"></div>
+				<div class="dhx_cal_data"></div>       
+			</div>
+		</div>			
 	</div>
 
 <?php PrintFooter($P); ?>
@@ -62,5 +59,6 @@
 		{name:"time", height:72, type:"time", map_to:"auto"}
 	]
 	
+	scheduler.init('scheduler_here', new Date(),"month");
 	scheduler.locale.labels.section_device="Device Name";
 </script>

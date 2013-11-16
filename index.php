ffc83def8a82d0133b08d22b617e1b70f848f286
nbonnet13 <thebonnet09@yahoo.com>
2013-11-15 18:03:24 -0600

    Fix for includes from the controller

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 include $_SERVER['DOCUMENT_ROOT'] . '/SmartSocket/includes/db_connection.php';
 
 if(isset($_GET['login'])) {
-	include '/SmartSocket/controllers/login_controller.php';
+	include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/controllers/login_controller.php';
 } elseif (isset($_GET['register'])) {
-    include '/SmartSocket/controllers/register_controller.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/controllers/register_controller.php';
 } elseif (isset($_GET['account'])) {
-	include '/SmartSocket/controllers/account_controller.php';
+	include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/controllers/account_controller.php';
 } elseif (isset($_GET['newDevice'])) {
-	include '/SmartSocket/controllers/newDevice_controller.php';
+	include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/controllers/newDevice_controller.php';
 } else {  
     include '404.html.php';  // No page found
 }

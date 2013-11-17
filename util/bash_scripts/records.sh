be4ad589d5dc483717d682e364bd413b708b9ab5
Ubuntu <server>
2013-11-17 20:45:39 +0000

    devices 1 and 2 simulate having a schedule

--- util/bash_scripts/records.sh
+++ util/bash_scripts/records.sh
@@ -4,4 +4,13 @@ VOLTAGE=$(( $RANDOM % 10 + 120 ));
 AMPS=$(( $RANDOM % 300 + 400 ));
 THE_TIME=`date +"%Y-%m-%d%%20%T"`;
 STATE=1;
-curl "http://yoursmartsocket.com/SmartSocket/insertrecord.php?dev_id=$1&time_id=$THE_TIME&amps=$AMPS&volts=$VOLTAGE&state=$STATE";
+THE_HOUR=`date +%H`
+MIN_HOUR=6
+MAX_HOUR=22
+if [ $1 -le 2 ]; then
+    if [ $THE_HOUR -gt $MIN_HOUR -a $THE_HOUR -lt $MAX_HOUR ]; then
+	curl "http://yoursmartsocket.com/SmartSocket/insertrecord.php?dev_id=$1&time_id=$THE_TIME&amps=$AMPS&volts=$VOLTAGE&state=$STATE";
+    fi
+else
+    curl "http://yoursmartsocket.com/SmartSocket/insertrecord.php?dev_id=$1&time_id=$THE_TIME&amps=$AMPS&volts=$VOLTAGE&state=$STATE";
+fi

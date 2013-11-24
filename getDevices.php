0f5559a9a8fae00a9e2a24e78736c3ad6efbbcc6
Ubuntu <server>
2013-11-24 23:49:01 +0000

    removed buttons that were covering up calendar header

--- getDevices.php
+++ getDevices.php
@@ -39,15 +39,15 @@ PrintHeader($P);
 <!-- ko ifnot: devices().length > 0-->
 <!-- /ko -->
 
-<div id="scheduler_here" class="dhx_cal_container" style='width:480px; height:360px; padding:10px;'>
+<div id="scheduler_here" class="dhx_cal_container" style='width:480px; height:420px; padding:10px;'>
   <div class="dhx_cal_navline">
     <div class="dhx_cal_prev_button">&nbsp;</div>
     <div class="dhx_cal_next_button">&nbsp;</div>
-    <div class="dhx_cal_today_button"></div>
+    <!--<div class="dhx_cal_today_button"></div>-->
     <div class="dhx_cal_date"></div>
-    <div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
+    <!--<div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
     <div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
-    <div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
+    <div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>-->
   </div>
   <div class="dhx_cal_header"></div>
   <div class="dhx_cal_data"></div>       

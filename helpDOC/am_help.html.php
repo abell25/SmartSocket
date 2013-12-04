e4b8df4ca3aba356db08a0ad2c96455c53d60861
nbonnet13 <thebonnet09@yahoo.com>
2013-12-03 18:51:36 -0600

    stupid mistake numero dos
    forgot the actual html files for the help doc

---
+++ helpDOC/am_help.html.php
@@ -0,0 +1,9 @@
+<?php
+	$content = "<div>
+					<h2>Account Management Help</h2>
+					<p>
+						You do this you do that
+					</p>
+				</div>";
+	include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/helpDOC.html.php';
+?>
---
+++ helpDOC/default.html.php
@@ -0,0 +1,9 @@
+<?php
+		$content = "<div>
+					<h2>Help Documentation</h2>
+					<p>
+						Welcome to the help documentation pages. Here you will find all the help you need to get started using your SmartSocket.
+					</p>
+				</div>";
+	include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/helpDOC.html.php';
+?>

e885b390c2ee7924116220a8ef2243afb6ce026b
nbonnet13 <thebonnet09@yahoo.com>
2013-12-03 19:44:39 -0600

    fixed some styling on help docs

--- helpDOC/default.html.php
+++ helpDOC/default.html.php
@@ -1,8 +1,11 @@
 <?php
 		$content = "<div>
 					<h2>Help Documentation</h2>
-					<p>
-						Welcome to the help documentation pages. Here you will find all the help you need to get started using your SmartSocket.
+					<p class='centerMe cushion_up'>
+						Welcome to the help documentation pages. 
+					</p>
+					<p class='centerMe'>
+						Here you will find all the help you need to get started using your SmartSocket.
 					</p>
 				</div>";
 	include $_SERVER['DOCUMENT_ROOT'].'/SmartSocket/helpDOC.html.php';

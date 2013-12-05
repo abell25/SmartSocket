4d10e09d89d8a700c73853774a62e74d2f18a0f6
SJAckerman <ackerma9@uwm.edu>
2013-12-04 21:17:59 -0600

    More syntax

--- php_scripts/generateSchedules.php
+++ php_scripts/generateSchedules.php
@@ -14,7 +14,7 @@ $sql = "SELECT id, start_date, end_date, text FROM events WHERE id = " . $device
 $result = mysql_query($sql) or die(mysql_error());
 $devices = array();
 
-$file = fopen("ScheduleFiles/" . */$deviceID . ".txt", "w+");
+$file = fopen("ScheduleFiles/" . $deviceID . ".txt", "w+");
 echo "File name: " . $deviceID;
 echo "<br>";
 

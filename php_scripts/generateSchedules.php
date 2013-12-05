eee5dfc02992735f95e356e80131cf2fa229bd9c
SJAckerman <ackerma9@uwm.edu>
2013-12-04 19:45:26 -0600

    Formatting changes

--- php_scripts/generateSchedules.php
+++ php_scripts/generateSchedules.php
@@ -1,18 +1,22 @@
 <?php
 //$user_id = $_SESSION['user_id'];
 
-function GenerateScheduleFiles()
+function GenerateScheduleFiles($id)
 {
 
 $conn = mysql_connect('localhost','root','bitnami') or die(mysql_error());
 mysql_select_db("SmartSocket") or die(mysql_error());
 
-$sql = "SELECT id, start_date, end_date, text FROM events WHERE id = 3";
+$sql = "SELECT id, start_date, end_date, text FROM events WHERE id = " . $id;
 
 //echo "sql: " . $sql . "<br />";
 
 $result = mysql_query($sql) or die(mysql_error());
 $devices = array();
+
+echo "File name: " . $id;
+echo "<br>";
+
 while ($row = mysql_fetch_assoc($result))
 {
 	echo $row['id'] . " " . $row['start_date'] . " " . $row['end_date'] . " " . $row['text'];

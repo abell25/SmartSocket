facf3ee6b57937c079a373a3e7d4ba61e62cc7c1
Ubuntu <server>
2013-12-06 15:32:22 +0000

    Fixed scheduler connector

--- scheduler/connector.php
+++ scheduler/connector.php
@@ -1,10 +1,14 @@
 <?php 
 include('connector/scheduler_connector.php');
- 
+
+session_start();
+$user_id = $_SESSION['user_id'];
+$sql = "SELECT * FROM events WHERE text IN (SELECT nickname FROM device WHERE user_id = $user_id)";
+  
 $res=mysql_connect('localhost','root','bitnami');
 mysql_select_db("smartsocket");
  
 $calender = new SchedulerConnector($res);
  
-$calender->render_table("events","id","start_date,end_date,text");
+$calender->render_sql($sql,"id","start_date,end_date,text");
 ?>

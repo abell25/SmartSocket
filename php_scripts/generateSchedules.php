e63922ee51f08898b9f9787551630dec9395cb61
pcrone <pjcrone@uwm.edu>
2013-12-08 15:40:03 -0600

    Update connector, removed day & week view, fixed pathing in generateSchedules

--- php_scripts/generateSchedules.php
+++ php_scripts/generateSchedules.php
@@ -5,12 +5,13 @@ function GenerateScheduleFile($deviceID)
 $conn = mysql_connect('localhost','root','bitnami') or die(mysql_error());
 mysql_select_db("SmartSocket") or die(mysql_error());
 
-$sql = "SELECT id, start_date, end_date, text FROM events WHERE device_id = " . $deviceID;
+$sql = 'SELECT id, start_date, end_date, text FROM events WHERE device_id = ' . $deviceID;
 
 $result = mysql_query($sql) or die(mysql_error());
 $devices = array();
 
-$file = fopen("ScheduleFiles/" . $deviceID . ".txt", "w+");
+$file = fopen("../ScheduleFiles/" . $deviceID . ".txt", "w+");
+
 
 while ($row = mysql_fetch_assoc($result))
 {
--- scheduler/connector.php
+++ scheduler/connector.php
@@ -4,7 +4,9 @@ include '../php_scripts/generateSchedules.php';
 
 session_start();
 $user_id = $_SESSION['user_id'];
-$sql = "SELECT * FROM events WHERE device_id IN (SELECT dev_id FROM device WHERE user_id = $user_id)";
+$sql = "SELECT e.id, e.start_date, e.end_date, d.nickname AS text, e.device_id
+         FROM events e JOIN device d ON e.device_id = d.dev_id
+         WHERE d.user_id = $user_id";
 
 $res=mysql_connect('localhost','root','bitnami');
 mysql_select_db("smartsocket");
@@ -21,7 +23,11 @@ $con_sched->event->attach("beforeInsert", "updateModified");
 $con_sched->event->attach("beforeUpdate", "updateModified");
 $con_sched->event->attach("beforeDelete", "updateModified");
 
-$con_sched->render_sql($sql,"id","start_date,end_date,text,device_id");
+if ($con_sched->is_select_mode())
+        $con_sched->render_complex_sql($sql,"id","start_date,end_date,text,device_id");
+else
+        $con_sched->render_table("events","id","start_date,end_date,text,device_id");
+
 ?>
 
 

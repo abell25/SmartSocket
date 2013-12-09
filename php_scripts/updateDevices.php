bea458f8e8235ad28b4dad5d5b75e35eeea73929
Ubuntu <server>
2013-12-09 21:44:42 +0000

    fixed bug where device wasn't being turned off.

--- php_scripts/updateDevices.php
+++ php_scripts/updateDevices.php
@@ -5,6 +5,7 @@ $dev_id = $_GET['dev_id'];
 
 $conn = mysql_connect('localhost','root','bitnami') or die(mysql_error());
 mysql_select_db("SmartSocket") or die(mysql_error());
+//echo "user_set_state = [".$_GET['user_set_state']."]\n";
 
 $sql = 'UPDATE device set ';
 $columns = array();
@@ -16,7 +17,7 @@ if (isset($_GET['max_power_usage']) and !empty($_GET['max_power_usage']))
   array_push($columns, "max_power_usage = " . $_GET['max_power_usage'] . ""); 
 if (isset($_GET['max_cost']) and !empty($_GET['max_cost']))
   array_push($columns, "max_cost = " . $_GET['max_cost'] . ""); 
-if (isset($_GET['user_set_state']) and !empty($_GET['user_set_state']))
+if (isset($_GET['user_set_state']))
   array_push($columns, "user_set_state = " . $_GET['user_set_state'] . ""); 
 
 $sql = $sql . implode(", ", $columns) . " where dev_id = $dev_id";
